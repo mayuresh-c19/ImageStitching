c7c643da8002f15ea2fd4ca70bcbe5367a62cecf
mayuresh-c19 <mayuresh1918@gmail.com>
2024-02-26 18:47:43 +0530

    Added Ransac for Homography

--- Image_Stitching.py
+++ Image_Stitching.py
@@ -7,7 +7,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 feature_extraction_algo = 'sift'
-feature_to_map = 'bf'
+feature_to_match = 'bf'
 
 # Make sure that the train image is the image that will be transformed
 train_photo = cv2.imread('./'  + 'train.jpg')
@@ -90,4 +90,199 @@ ax2.imshow(cv2.drawKeypoints(query_photo_gray,keypoints_query_img,None,color=(0,
 ax2.set_xlabel("(b)", fontsize=14)
 
 plt.savefig("./output/" + feature_extraction_algo + "_features_img_"+'.jpeg', bbox_inches='tight')
+plt.show()
+
+def create_matching_object(method,crossCheck):
+    "Create and return a Matcher Object"
+    
+    # For BF matcher, first we have to create the BFMatcher object using cv2.BFMatcher(). 
+    # It takes two optional params. 
+    # normType - It specifies the distance measurement
+    # crossCheck - which is false by default. If it is true, Matcher returns only those matches 
+    # with value (i,j) such that i-th descriptor in set A has j-th descriptor in set B as the best match 
+    # and vice-versa. 
+    if method == 'sift' or method == 'surf':
+        bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=crossCheck)
+    elif method == 'orb' or method == 'brisk':
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=crossCheck)
+    return bf
+
+def key_points_matching(features_train_img, features_query_img, method):
+    """
+    Perform key points matching between the features of a train image and a query image using a specified method.
+
+    Parameters:
+    features_train_img (np.ndarray): Features of the train image.
+    features_query_img (np.ndarray): Features of the query image.
+    method (str): Method to use for matching.
+
+    Returns:
+    rawMatches (list): List of raw matches between the train and query images.
+    """
+    bf = create_matching_object(method, crossCheck=True)
+        
+    # Match descriptors.
+    best_matches = bf.match(features_train_img,features_query_img)
+    
+    # Sort the features in order of distance.
+    # The points with small distance (more similarity) are ordered first in the vector
+    rawMatches = sorted(best_matches, key = lambda x:x.distance)
+    print("Raw matches with Brute force):", len(rawMatches))
+    return rawMatches
+
+def key_points_matching_KNN(features_train_img, features_query_img, ratio, method):
+    """
+    Perform key points matching using K-Nearest Neighbors (KNN) algorithm.
+
+    Args:
+        features_train_img (list): List of features from the training image.
+        features_query_img (list): List of features from the query image.
+        ratio (float): Ratio threshold for Lowe's ratio test.
+        method (string): Method for creating the matching object.
+
+    Returns:
+        list: List of matches between key points in the training and query images.
+    """
+    bf = create_matching_object(method, crossCheck=False)
+    # compute the raw matches and initialize the list of actual matches
+    rawMatches = bf.knnMatch(features_train_img, features_query_img, k=2)
+    print("Raw matches (knn):", len(rawMatches))
+    matches = []
+
+    # loop over the raw matches
+    for m,n in rawMatches:
+        # ensure the distance is within a certain ratio of each
+        # other (i.e. Lowe's ratio test)
+        if m.distance < n.distance * ratio:
+            matches.append(m)
+    return matches
+
+# print("Drawing: {} matched features Lines".format(feature_to_match))
+
+# fig = plt.figure(figsize=(20,8))
+
+# if feature_to_match == 'bf':
+#     matches = key_points_matching(features_train_img, features_query_img, method=feature_extraction_algo)
+    
+#     mapped_features_image = cv2.drawMatches(train_photo,keypoints_train_img,query_photo,keypoints_query_img,matches[:100],
+#                            None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+# # Now for cross checking draw the feature-mapping lines also with KNN
+# elif feature_to_match == 'knn':
+#     matches = key_points_matching_KNN(features_train_img, features_query_img, ratio=0.75, method=feature_extraction_algo)
+    
+#     mapped_features_image_knn = cv2.drawMatches(train_photo, keypoints_train_img, query_photo, keypoints_query_img, np.random.choice(matches,100),
+#                            None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    
+
+# plt.imshow(mapped_features_image)
+# plt.axis('off')
+# plt.savefig("./output/" + feature_to_match + "_matching_img_"+'.jpeg', bbox_inches='tight', 
+#             dpi=300, format='jpeg')
+# plt.show()
+
+feature_to_match = 'knn'
+
+print("Drawing: {} matched features Lines".format(feature_to_match))
+
+fig = plt.figure(figsize=(20,8))
+
+if feature_to_match == 'bf':
+    matches = key_points_matching(features_train_img, features_query_img, method=feature_extraction_algo)
+    
+    mapped_features_image = cv2.drawMatches(train_photo,keypoints_train_img,query_photo,keypoints_query_img,matches[:100],
+                           None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+# Now for cross checking draw the feature-mapping lines also with KNN
+elif feature_to_match == 'knn':
+    matches = key_points_matching_KNN(features_train_img, features_query_img, ratio=0.75, method=feature_extraction_algo)
+    
+    mapped_features_image_knn = cv2.drawMatches(train_photo, keypoints_train_img, query_photo, keypoints_query_img, np.random.choice(matches,100),
+                           None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    
+
+plt.imshow(mapped_features_image_knn)
+plt.axis('off')
+plt.savefig("./output/" + feature_to_match + "_matching_img_"+'.jpeg', bbox_inches='tight', 
+            dpi=300, format='jpeg')
+plt.show()
+
+def homography_stitching(keypoints_train_img, keypoints_query_img, matches, reprojThresh):   
+    """ 
+    
+    Perform homography stitching by calculating the homography matrix.
+    converting the keypoints to numpy arrays before passing them for calculating Homography Matrix.
+    
+    Because we are supposed to pass 2 arrays of coordinates to cv2.findHomography, as in I have these points in image-1, and I have points in image-2, so now what is the homography matrix to transform the points from image 1 to image 2
+
+    Args:
+        keypoints_train_img (list): List of keypoints from the training image.
+        keypoints_query_img (list): List of keypoints from the query image.
+        matches (list): List of matches between key points in the training and query images.
+        reprojThresh (float): Reprojection threshold for the RANSAC algorithm.
+
+    Returns:
+        tuple: Tuple containing the matches, homography matrix, and status.
+               - matches (list): List of matches between key points in the training and query images.
+               - H (numpy.ndarray): Homography matrix.
+               - status (numpy.ndarray): Status of inlier points.
+
+    Note:
+        The minimum number of matches required for calculating the homography is 4.
+    
+    
+    """
+    keypoints_train_img = np.float32([keypoint.pt for keypoint in keypoints_train_img])
+    keypoints_query_img = np.float32([keypoint.pt for keypoint in keypoints_query_img])
+    
+    ''' For findHomography() - I need to have an assumption of a minimum of correspondence points that are present between the 2 images. Here, I am assuming that Minimum Match Count to be 4 '''
+    if len(matches) > 4:
+        # construct the two sets of points
+        points_train = np.float32([keypoints_train_img[m.queryIdx] for m in matches])
+        points_query = np.float32([keypoints_query_img[m.trainIdx] for m in matches])
+        
+        # Calculate the homography between the sets of points
+        (H, status) = cv2.findHomography(points_train, points_query, cv2.RANSAC, reprojThresh)
+
+        return (matches, H, status)
+    else:
+        return None
+
+M = homography_stitching(keypoints_train_img, keypoints_query_img, matches, reprojThresh=4)
+
+if M is None:
+    print("Error!")
+
+(matches, Homography_Matrix, status) = M
+
+print(Homography_Matrix)
+
+# For the calculation of the width and height of the final horizontal panoramic images 
+# I can just add the widths of the individual images and for the height
+# I can take the max from the 2 individual images.
+
+width = query_photo.shape[1] + train_photo.shape[1]
+print("width ", width) 
+# 2922 - Which is exactly the sum value of the width of 
+# my train.jpg and query.jpg
+
+
+height = max(query_photo.shape[0], train_photo.shape[0])
+
+# otherwise, apply a perspective warp to stitch the images together
+
+# Now just plug that "Homography_Matrix"  into cv::warpedPerspective and I shall have a warped image1 into image2 frame
+
+result = cv2.warpPerspective(train_photo, Homography_Matrix,  (width, height))
+
+# The warpPerspective() function returns an image or video whose size is the same as the size of the original image or video. Hence set the pixels as per my query_photo
+
+result[0:query_photo.shape[0], 0:query_photo.shape[1]] = query_photo
+
+plt.figure(figsize=(20,10))
+plt.axis('off')
+plt.imshow(result)
+
+imageio.imwrite("./output/horizontal_panorama_img_"+'.jpeg', result)
+
 plt.show()
