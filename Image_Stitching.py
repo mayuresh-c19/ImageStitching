81abaa7d6df4ce6d44c34299790d6efe29a18509
mayuresh-c19 <mayuresh1918@gmail.com>
2024-02-26 18:38:56 +0530

    Created Keypoints on Train Images

--- Image_Stitching.py
+++ Image_Stitching.py
@@ -6,7 +6,7 @@ cv2.ocl.setUseOpenCL(False)
 import warnings
 warnings.filterwarnings('ignore')
 
-feature_exraction_algo = 'sift'
+feature_extraction_algo = 'sift'
 feature_to_map = 'bf'
 
 # Make sure that the train image is the image that will be transformed
@@ -33,4 +33,61 @@ ax2.imshow(train_photo, cmap="gray")
 ax2.set_xlabel("Train image (Image to be transformed)", fontsize=14)
 
 plt.savefig("./plottings"+'.jpeg', bbox_inches='tight', dpi=300, format='jpeg')
+plt.show()
+
+def select_descriptor_methods(image, method=None):    
+    
+    assert method is not None, "Please define a feature descriptor method. accepted Values are: 'sift', 'surf'"
+    
+    if method == 'sift':
+        descriptor = cv2.SIFT_create()
+    elif method == 'surf':
+        descriptor = cv2.SURF_create()
+    elif method == 'brisk':
+        descriptor = cv2.BRISK_create()
+    elif method == 'orb':
+        descriptor = cv2.ORB_create()
+        
+    (keypoints, features) = descriptor.detectAndCompute(image, None)
+    
+    return (keypoints, features)
+
+keypoints_train_img, features_train_img = select_descriptor_methods(train_photo_gray, method=feature_extraction_algo)
+
+keypoints_query_img, features_query_img = select_descriptor_methods(query_photo_gray, method=feature_extraction_algo)
+
+for keypoint in keypoints_query_img:
+    x,y = keypoint.pt
+    size = keypoint.size 
+    orientation = keypoint.angle
+    response = keypoint.response 
+    octave = keypoint.octave
+    class_id = keypoint.class_id
+
+
+print (x,y)
+ 
+print(size)
+
+print(orientation)
+
+print(response)
+print(octave)
+print(class_id)
+
+print(len(keypoints_query_img))
+
+features_query_img.shape
+
+# display the keypoints and features detected on both images
+fig, (ax1,ax2) = plt.subplots(nrows=1, ncols=2, figsize=(20,8), constrained_layout=False)
+
+ax1.imshow(cv2.drawKeypoints(train_photo_gray, keypoints_train_img, None, color=(0,255,0)))
+
+ax1.set_xlabel("(a)", fontsize=14)
+
+ax2.imshow(cv2.drawKeypoints(query_photo_gray,keypoints_query_img,None,color=(0,255,0)))
+ax2.set_xlabel("(b)", fontsize=14)
+
+plt.savefig("./output/" + feature_extraction_algo + "_features_img_"+'.jpeg', bbox_inches='tight')
 plt.show()
